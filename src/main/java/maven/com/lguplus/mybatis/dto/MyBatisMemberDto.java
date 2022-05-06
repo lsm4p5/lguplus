07782c8af897091eb3ae3b68bde2d2ee4ad8ed1e
lsm4p5 <lsm4p5@gmail.com>
2022-05-06 23:27:45 +0900

    다시 commit

---
+++ src/main/java/maven/com/lguplus/mybatis/dto/MyBatisMemberDto.java
@@ -0,0 +1,11 @@
+package maven.com.lguplus.mybatis.dto;
+
+
+
+public class MyBatisMemberDto {
+
+    private String loginname;
+    private String username;
+    private String password;
+    private int age;
+}
